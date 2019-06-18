d9e26fe2f9d14ecf0d10a6acfcae38e9821cf830
pravin1390 <33753834+pravin1390@users.noreply.github.com>
2019-06-18 10:05:29 +0530

    Update nlm_approx.m
    Changes done to make sure that image does not get saturated. This is important to calculate the metrics. Displaying is fine as matlab takes care of it by clipping values to range [0,255].

--- nlm_approx.m
+++ nlm_approx.m
@@ -41,6 +41,8 @@ spatialtype='box';
 convmethod='O1'; % 'matlab' for matlab convolutions and 'O1' for O(1) convolutions
 Ikmean=fastKmeansfiltapprox(I2,S,3.5*sigma/256,Centre,Clusterindex,spatialtype,convmethod,fast_flag,Apca);      % nlm kmeans
 Ikmean=Ikmean.*255;
+Ikmean(Ikmean<0)=0;
+Ikmean(Ikmean>255)=255;
 toc
 Tkmeans=toc;
 fprintf('non local means by Kmeans complete with %d clusters \n',Cluster);
